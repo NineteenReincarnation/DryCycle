8123da769f1cb2da77896f37084515dfb25daa51
NineteenReincarnation <164201860+NineteenReincarnation@users.noreply.github.com>
2026-08-27 03:03:29 +0800

    Bump DryCycle to 0.2.2

--- src/DryCycle/Plugin.cs
+++ src/DryCycle/Plugin.cs
@@ -8,7 +8,7 @@ public sealed class Plugin : BaseUnityPlugin
 {
     public const string ModId = "Anno";
     public const string ModName = "DryCycle";
-    public const string Version = "0.2.1";
+    public const string Version = "0.2.2";
 
     private bool _initialized;
 
