2c4e1bc8f2bc2e935f4580d42674d76537f05002
NineteenReincarnation <164201860+NineteenReincarnation@users.noreply.github.com>
2026-08-27 03:17:17 +0800

    Use standard Rain World BepInEx plugin bootstrap

---
+++ src/Plugin.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Security.Permissions;
+using BepInEx;
+using BepInEx.Logging;
+using DryCycle.Thirst;
+
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace DryCycle;
+
+[BepInPlugin(ModId, ModName, Version)]
+internal sealed class Plugin : BaseUnityPlugin
+{
+    public const string ModId = "Anno";
+    public const string ModName = "DryCycle";
+    public const string Version = "0.2.3";
+
+    internal new static ManualLogSource Logger;
+    private static bool _initialized;
+
+    public void OnEnable()
+    {
+        Logger = base.Logger;
+        On.RainWorld.OnModsInit += RainWorld_OnModsInit;
+    }
+
+    public void OnDisable()
+    {
+        On.RainWorld.OnModsInit -= RainWorld_OnModsInit;
+
+        if (_initialized)
+        {
+            ThirstHooks.Disable();
+            _initialized = false;
+        }
+    }
+
+    private static void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+
+        if (_initialized)
+        {
+            return;
+        }
+
+        try
+        {
+            ThirstHooks.Enable();
+            _initialized = true;
+            Logger.LogInfo($"{ModName} {Version}: thirst system enabled.");
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+            throw;
+        }
+    }
+}
