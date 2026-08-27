b1ded70fa17852fac3efce299b0472e9271fb9f8
NineteenReincarnation <164201860+NineteenReincarnation@users.noreply.github.com>
2026-08-27 13:42:32 +0800

    Add Dew Pod abstract consumable

---
+++ src/Items/DewPod/AbstractDewPod.cs
@@ -0,0 +1,54 @@
+using System.Globalization;
+using UnityEngine;
+
+namespace DryCycle.Items.DewPod;
+
+internal sealed class AbstractDewPod : AbstractConsumable
+{
+    public const float MaxWaterWV = 500f;
+
+    public float WaterWV;
+    public bool Broken;
+
+    public AbstractDewPod(
+        World world,
+        WorldCoordinate pos,
+        EntityID id,
+        int originRoom,
+        int placedObjectIndex,
+        PlacedObject.ConsumableObjectData consumableData,
+        float waterWV = MaxWaterWV,
+        bool broken = false)
+        : base(
+            world,
+            DewPodHooks.ObjectType,
+            null,
+            pos,
+            id,
+            originRoom,
+            placedObjectIndex,
+            consumableData)
+    {
+        WaterWV = Mathf.Clamp(waterWV, 0f, MaxWaterWV);
+        Broken = broken;
+    }
+
+    public override string ToString()
+    {
+        string baseString = string.Format(
+            CultureInfo.InvariantCulture,
+            "{0}<oA>{1}<oA>{2}<oA>{3}<oA>{4}" +
+            "<oA>DRYCYCLE_DEWPOD_WATER={5}" +
+            "<oA>DRYCYCLE_DEWPOD_BROKEN={6}",
+            base.IDAndRippleLayerString,
+            type.ToString(),
+            pos.SaveToString(),
+            originRoom,
+            placedObjectIndex,
+            WaterWV,
+            Broken ? 1 : 0);
+
+        baseString = SaveState.SetCustomData(this, baseString);
+        return SaveUtils.AppendUnrecognizedStringAttrs(baseString, "<oA>", unrecognizedAttributes);
+    }
+}
