679e556b7fc3b58c8805d9e8a07d40398e068241
NineteenReincarnation <164201860+NineteenReincarnation@users.noreply.github.com>
2026-08-27 11:56:25 +0800

    Bump version to 0.0.33

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -17,7 +17,7 @@ internal sealed class Plugin : BaseUnityPlugin
 {
     public const string ModId = "Anno";
     public const string ModName = "DryCycle";
-    public const string Version = "0.0.32";
+    public const string Version = "0.0.33";
 
     internal new static ManualLogSource Logger;
     private static bool _initialized;
