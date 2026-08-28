2672edbcf9ce3122d3b5d30c328b95c77d839aaf
NineteenReincarnation <164201860+NineteenReincarnation@users.noreply.github.com>
2026-08-28 23:49:44 +0800

    Mark beta snapshot as 0.1.46

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -21,7 +21,7 @@ internal sealed class Plugin : BaseUnityPlugin
 {
     public const string ModId = "Anno";
     public const string ModName = "DryCycle";
-    public const string Version = "0.0.46";
+    public const string Version = "0.1.46";
 
     internal new static ManualLogSource Logger;
     private static bool _initialized;
