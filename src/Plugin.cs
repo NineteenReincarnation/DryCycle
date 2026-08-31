e7225db8181a65ae25dc3d5d1eb36d77b44da0b8
NineteenReincarnation <164201860+NineteenReincarnation@users.noreply.github.com>
2026-08-31 23:09:58 +0800

    Bump version for weather scheduler

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -27,7 +27,7 @@ internal sealed class Plugin : BaseUnityPlugin
 {
     public const string ModId = "Anno";
     public const string ModName = "DryCycle";
-    public const string Version = "0.1.85";
+    public const string Version = "0.1.86";
 
     internal new static ManualLogSource Logger;
     private static bool _contentRegistered;
