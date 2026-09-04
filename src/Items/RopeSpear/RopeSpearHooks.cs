cb1905728f347777259f934affafe0ac5b280619
NineteenReincarnation <164201860+NineteenReincarnation@users.noreply.github.com>
2026-09-04 20:47:14 +0800

    Fix missing RWCustom namespace in RopeSpear hooks

--- src/Items/RopeSpear/RopeSpearHooks.cs
+++ src/Items/RopeSpear/RopeSpearHooks.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
+using RWCustom;
 using UnityEngine;
 
 namespace DryCycle.Items.RopeSpear;
