6701d73d07d5faa9764cd589ba4abac3af605631
Orrhisk <164201860+NineteenReincarnation@users.noreply.github.com>
2026-09-07 13:02:11 +0800

    Register Task 13 environmental Observatory

--- src/Creatures/DesertBatfly/DesertBatflyHooks.cs
+++ src/Creatures/DesertBatfly/DesertBatflyHooks.cs
@@ -29,7 +29,7 @@ internal static class DesertBatflyHooks
         DesertBatflyPlatformRoostRuntime.Enable();
         if (!debugRegistered)
         {
-            AIDebugRegistry.Register(new DesertBatflyTask12DebugSource());
+            AIDebugRegistry.Register(new DesertBatflyTask13DebugSource());
             debugRegistered = true;
         }
         On.Fly.ReportToFliesRoomAI += Report;
