1cd1a095b83d2754c1a72e4ef753142e34feea9f
Orrhisk <164201860+NineteenReincarnation@users.noreply.github.com>
2026-09-08 23:06:00 +0800

    Fix missing semantic imports after Task14 merge

---
+++ src/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using System.Collections.Generic;
+global using Vector2 = UnityEngine.Vector2;
+global using Mathf = UnityEngine.Mathf;
+global using Custom = RWCustom.Custom;
+global using Appendage = PhysicalObject.Appendage;
