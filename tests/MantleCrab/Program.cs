785727b338a9ca417bcf262dacbc19d4296f24e2
Orrhisk <164201860+NineteenReincarnation@users.noreply.github.com>
2026-09-10 12:12:07 +0800

    Run MantleCrab material regression tests

--- tests/MantleCrab/Program.cs
+++ tests/MantleCrab/Program.cs
@@ -28,6 +28,7 @@ internal static partial class Program
             TerrainTests();
             PlatformSurfaceTests();
             PlatformRuntimeTests();
+            MaterialTests();
             GeometryTests();
 
             Console.WriteLine(
