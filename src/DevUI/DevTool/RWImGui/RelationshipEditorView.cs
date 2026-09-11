150ba5380c20b86e8cb2093f2462df91bbc12961
Orrhisk <164201860+NineteenReincarnation@users.noreply.github.com>
2026-09-11 20:14:40 +0800

    DevTool: make relationship workspace movable and resizable

--- src/DevUI/DevTool/RWImGui/RelationshipEditorView.cs
+++ src/DevUI/DevTool/RWImGui/RelationshipEditorView.cs
@@ -60,11 +60,11 @@ internal static class RelationshipEditorView
     {
         if (!snapshot.Available || size.X < 180f || size.Y < 120f) return;
 
-        ImGui.SetNextWindowPos(position, ImGuiCond.Always);
-        ImGui.SetNextWindowSize(size, ImGuiCond.Always);
+        ImGui.SetNextWindowPos(position, ImGuiCond.FirstUseEver);
+        ImGui.SetNextWindowSize(size, ImGuiCond.FirstUseEver);
+        ImGui.SetNextWindowSizeConstraints(new Num.Vector2(520f, 280f), new Num.Vector2(4000f, 4000f));
         ImGui.SetNextWindowBgAlpha(0.98f);
-        ImGuiWindowFlags flags = ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoMove |
-                                 ImGuiWindowFlags.NoSavedSettings;
+        ImGuiWindowFlags flags = ImGuiWindowFlags.NoCollapse;
         if (!ImGui.Begin("Relationships###DevToolRelationshipMatrix", flags))
         {
             ImGui.End();
