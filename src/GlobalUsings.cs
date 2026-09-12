156694ca070cdcfdb6ad87edabe7e5d8d6d3420a
Orrhisk <164201860+NineteenReincarnation@users.noreply.github.com>
2026-09-13 01:38:57 +0800

    Expose BezierSpline alias for geometry-aware map thumbnails

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -2,4 +2,5 @@ global using System.Collections.Generic;
 global using Vector2 = UnityEngine.Vector2;
 global using Mathf = UnityEngine.Mathf;
 global using Custom = RWCustom.Custom;
+global using BezierSpline = RWCustom.BezierSpline;
 global using Appendage = PhysicalObject.Appendage;
