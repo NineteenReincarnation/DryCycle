fadde60c6f4df2c138e2abb2631e98712f508ecb
Orrhisk <164201860+NineteenReincarnation@users.noreply.github.com>
2026-09-13 01:42:24 +0800

    Keep BezierSpline import local to World Map geometry

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -2,5 +2,4 @@ global using System.Collections.Generic;
 global using Vector2 = UnityEngine.Vector2;
 global using Mathf = UnityEngine.Mathf;
 global using Custom = RWCustom.Custom;
-global using BezierSpline = RWCustom.BezierSpline;
 global using Appendage = PhysicalObject.Appendage;
