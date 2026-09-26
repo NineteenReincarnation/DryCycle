10e83e39291cb96a091bb2bdd85a2bc07248c234
Orrhisk <164201860+NineteenReincarnation@users.noreply.github.com>
2026-09-26 22:16:09 +0800

    Match cartography room border thickness to Cornifer

--- src/DevUI/DevTool/Map/Cartography/CartographyAppearance.cs
+++ src/DevUI/DevTool/Map/Cartography/CartographyAppearance.cs
@@ -30,7 +30,7 @@ internal sealed class CartographyOptions
     public bool TileWalls = true, Borders = true, MarkShortcuts = true, ExitsOnly = true, ShortcutBackground = true;
     public bool Objects = true, Pickups, Slugcats, Diamonds = true, HollowDiamonds, SpecialRooms = true, InRoomShortcuts = true;
     public uint Canvas = 0x00000000, RoomNameColor = 0xFFFFFF00, Wall = 0xFF050505;
-    public float WaterOpacity = .3f, BorderSize = 3;
+    public float WaterOpacity = .3f, BorderSize = 5;
     public string HiddenTypes = "DevToken", Campaign = "White", Installation = "", SpriteDirectory = "";
     public string PanKey = "Right", DeleteKey = "Delete", DuplicateKey = "Ctrl+D", CopyKey = "Ctrl+C", CutKey = "Ctrl+X", PasteKey = "Ctrl+V";
     public bool ExportArea;
